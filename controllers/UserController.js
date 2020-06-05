98ed2622375511bca061b82e6cd674111360f5f0
joao Paulo <joaopaulozangrande@gmail.com>
2020-06-05 19:50:05 -0300

    i fixed some bugs in the code

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -60,7 +60,11 @@ class UserController {
                 reject(e);
             }
 
-            fileReader.readAsDataURL(file);
+            if (file){
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
 
         })
 
@@ -78,7 +82,11 @@ class UserController {
                     user[field.name] = field.value;
                 }
 
-            }else {
+            }else if (field.name == "admin"){
+
+                user[field.name] = field.checked;
+
+            } else {
 
                 user[field.name] = field.value;
 
@@ -102,19 +110,21 @@ class UserController {
 
     addLine(dataUser){
 
-        this.tableEl.innerHTML = `
-                <tr>
-                    <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
-                    <td>${dataUser.name}</td>
-                    <td>${dataUser.email}</td>
-                    <td>${dataUser.admin}</td>
-                    <td>${dataUser.birth}</td>
-                    <td>
-                      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                    </td>
-                </tr>
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+              <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+              <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
     `;
+
+        this.tableEl.appendChild(tr);
 
     }
 
