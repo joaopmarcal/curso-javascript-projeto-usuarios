65895984f39a28a88c8ef9957ba36aeb289ab3b9
joao Paulo <joaopaulozangrande@gmail.com>
2020-06-05 19:20:46 -0300

    i used promisses im my project

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -17,37 +17,52 @@ class UserController {
 
             let values = this.getValues();
 
-            this.getPhoto((content)=>{
+            this.getPhoto().then(
 
-                values.photo = content;
+                (content) => {
 
-                this.addLine(values);
+                    values.photo = content;
 
-            });
+                    this.addLine(values);
+
+                },
+                (e) => {
+                    console.error(e)
+                }
+
+            );
 
         });
 
     }
 
-    getPhoto(callback){
+    getPhoto(){
 
-        let fileReader = new FileReader();
+        return new Promise ( (resolve, reject)=>{
 
-        let elements = [...this.formEl.elements].filter(item=>{
-            if (item.name === 'photo'){
-                return item;
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+                if (item.name === 'photo'){
+                    return item;
+                }
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = ()=>{
+
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e)=>{
+                reject(e);
             }
-        });
 
-        let file = elements[0].files[0];
+            fileReader.readAsDataURL(file);
 
-        fileReader.onload = ()=>{
-
-            callback(fileReader.result);
-
-        };
-
-        fileReader.readAsDataURL(file);
+        })
 
     }
 
